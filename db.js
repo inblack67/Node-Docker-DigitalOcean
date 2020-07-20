4c51eca3f82db3754e85dde6e43e530ef2f99349
Aman <devproddeploy@gmail.com>
2020-07-21 00:31:57 +0530

    docker is up

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://ryuwaki:watari@files-axoui.mongodb.net/mongo-multer?retryWrites=true&w=majority', {
+        await mongoose.connect(process.env.MONGO_URI, {
             useCreateIndex: true,
             useFindAndModify: false,
             useNewUrlParser: true,
